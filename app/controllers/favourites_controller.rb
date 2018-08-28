dbbc4483881e9bb8b05394b9b05463aab81c086e
kitsera <sashakitsera16358@gmail.com>
2018-08-28 23:55:52 +0300

    Need to implement getting urls from api.

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -4,20 +4,42 @@ require 'json'
 class FavouritesController < ApplicationController
 
   def index
-    @url ='https://api.thecatapi.com/v1/favourites'
-    @api_key = '3758c3ed-ad4f-4fa7-b181-746071587ee2'
-    uri = URI.parse(@url)
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    header = {'Content-Type': 'application/json', 'x-api-key': @api_key}
-    request = Net::HTTP::Get.new(uri, header)
-    response = http.request(request)
-    @resp = JSON.parse(response.read_body)
+    def request_to_api(url)
+      @api_key = '3758c3ed-ad4f-4fa7-b181-746071587ee2'
+      uri = URI.parse(url)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      header = {'Content-Type': 'application/json', 'x-api-key': @api_key}
+      req = Net::HTTP::Get.new(uri, header)
+      response = http.request(req)
+      JSON.parse(response.read_body)
+    end
+
+    url_fav = 'https://api.thecatapi.com/v1/favourites/'
+    url_search = 'https://api.thecatapi.com/v1/images/'
+    @resp = request_to_api(url_fav)
+
+    @resp.map! { |e| e['image_id'] }
+    urls =  []
+
+    @resp.each do |e|
+      url = request_to_api(url_search + e.to_s)
+      puts url
+      puts url['message'].nil?
+      if url['message'].nil?
+        puts 'Some deletion code'
+      else
+        @urls << url['url']
+      end
+      puts @urlss
+      #@urls << url['url']
+    end
+
+
   end
 
   def new
     @user_id = 'nfzh4j'
-    puts 'HAHAHA'
   end
 
   def create
